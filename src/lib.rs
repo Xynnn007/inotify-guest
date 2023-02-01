175eb38a810f259d8849c13f7011517cfa49719f
Xynnn007 <xynnn@linux.alibaba.com>
2023-02-01 12:19:22 +0800

    fix: inotify should be infinite
    use stream instead of a single future
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 //! use to inotify the creation of new tdx guest's vsocks
 
-use std::{collections::BTreeMap, ffi::OsStr};
+use std::{collections::BTreeMap, ffi::OsString};
 
 use anyhow::*;
+use futures::TryStreamExt;
 use inotify::{Event, EventMask, Inotify, WatchMask};
 use log::{info, warn};
 use tokio::{io, net::UnixStream, task::JoinHandle};
@@ -33,12 +34,20 @@ impl Multiplexer {
             .context("watch failed")?;
 
         let mut buffer = [0; 4096];
-        let events = self
+        let mut events = self
             .inotify
-            .read_events_blocking(&mut buffer)
+            .event_stream(&mut buffer)
             .context("Error while reading events")?;
 
-        for event in events {
+        while let std::result::Result::Ok(event) = events.try_next().await {
+            let event = match event {
+                Some(e) => e,
+                None => {
+                    warn!("get an empty event.");
+                    continue;
+                }
+            };
+
             if event.mask.contains(EventMask::CREATE) {
                 let name = get_guest_id(event)?;
                 if name.ends_with(VSOCK_SUFFIX) && name.starts_with(VSOCK_PREFIX) {
@@ -46,6 +55,7 @@ impl Multiplexer {
                         .trim_start_matches(VSOCK_PREFIX)
                         .trim_end_matches(VSOCK_SUFFIX);
 
+                    info!("Create new guest id {id}");
                     let qgs_socket = UnixStream::connect(qgs_socket_path)
                         .await
                         .context("dragonball unix socket bind")?;
@@ -66,7 +76,7 @@ impl Multiplexer {
                             async {
                                 io::copy(&mut guest_r, &mut qgs_w)
                                     .await
-                                    .context("qgs  send guest response failed")
+                                    .context("qgs send guest response failed")
                             }
                         );
                     });
@@ -75,10 +85,14 @@ impl Multiplexer {
                 }
             } else if event.mask.contains(EventMask::DELETE) {
                 let name = get_guest_id(event)?;
+
                 if name.ends_with(VSOCK_SUFFIX) && name.starts_with(VSOCK_PREFIX) {
                     let id = name
                         .trim_start_matches(VSOCK_PREFIX)
                         .trim_end_matches(VSOCK_SUFFIX);
+
+                    info!("Remove guest id {id}");
+
                     match self.qgs_map.get_mut(id) {
                         Some(slot) => {
                             slot.abort();
@@ -94,7 +108,7 @@ impl Multiplexer {
     }
 }
 
-fn get_guest_id(event: Event<&OsStr>) -> Result<String> {
+fn get_guest_id(event: Event<OsString>) -> Result<String> {
     let res = event
         .name
         .ok_or_else(|| anyhow!("inotify catches empty filename"))?
